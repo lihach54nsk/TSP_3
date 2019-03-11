7d5c4567770530460c551f86647f3b902a3c78f9
Luzyanin Alexey <261-96@mail.ru>
2019-03-11 19:50:42 +0700

    front

--- TSP_3/Form1.Designer.cs
+++ TSP_3/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.S1TextBox = new System.Windows.Forms.TextBox();
             this.S2TextBox = new System.Windows.Forms.TextBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.button1 = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -102,25 +102,26 @@
             series2.Legend = "Legend1";
             series2.Name = "Series1";
             this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.Size = new System.Drawing.Size(404, 300);
             this.chart1.TabIndex = 6;
             this.chart1.Text = "chart1";
             // 
-            // button1
+            // StartButton
             // 
-            this.button1.Location = new System.Drawing.Point(12, 87);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(245, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.StartButton.Location = new System.Drawing.Point(12, 87);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(245, 94);
+            this.StartButton.TabIndex = 7;
+            this.StartButton.Text = "Симуляция";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(882, 314);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(676, 314);
+            this.Controls.Add(this.StartButton);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.S2TextBox);
             this.Controls.Add(this.S1TextBox);
@@ -145,7 +146,7 @@
         private System.Windows.Forms.TextBox S1TextBox;
         private System.Windows.Forms.TextBox S2TextBox;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button StartButton;
     }
 }
 
--- TSP_3/Form1.cs
+++ TSP_3/Form1.cs
@@ -12,9 +12,11 @@ namespace TSP_3
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1() => InitializeComponent();
+
+        private void StartButton_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
+
         }
     }
 }
